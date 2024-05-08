47355ac78291a341753e48418f400dedbba19f08
OksanaMykytyn <128389515+OksanaMykytyn@users.noreply.github.com>
2024-05-08 23:57:56 +0300

    Update SweetController.php

--- my-gift/app/Http/Controllers/Api/SweetController.php
+++ my-gift/app/Http/Controllers/Api/SweetController.php
@@ -9,6 +9,33 @@ use Illuminate\Http\Request;
 
 class SweetController extends Controller
 {
+    public function addToCart($id) {
+        $sweet = Sweet::findOrFail($id);
+        $cart = session()->get('cart', []);
+    
+        if (isset($cart[$sweet])) {
+            return response()->json(['message' => 'Sweet already exists in the cart'], 400);
+        }
+    
+    
+        $cart[$sweet] = $sweet;
+        session()->put('cart', $cart);
+    
+        return response()->json(['message' => 'Sweet added to cart'], 200);
+    }
+
+    public function removeFromCart($id) {
+        $sweet = Sweet::findOrFail($id);
+        $cart = session()->get('cart', []);
+    
+        if (isset($cart[$sweet])) {
+            unset($cart[$sweet]);
+        session()->put('cart', $cart);
+    
+        return response()->json(['message' => 'Sweet removed from cart'], 200);
+        }
+
+    }
     /**
      * Display a listing of the resource.
      */
