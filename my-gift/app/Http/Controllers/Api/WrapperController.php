3f6a69a150dd517664478c5f4459f5c1b966dde5
OksanaMykytyn <128389515+OksanaMykytyn@users.noreply.github.com>
2024-05-08 23:58:27 +0300

    Update WrapperController.php

--- my-gift/app/Http/Controllers/Api/WrapperController.php
+++ my-gift/app/Http/Controllers/Api/WrapperController.php
@@ -9,6 +9,32 @@ use Illuminate\Http\Request;
 
 class WrapperController extends Controller
 {
+    public function addToCart($id) {
+        $wrapper = Wrapper::findOrFail($id);
+        $cart = session()->get('cart', []);
+    
+        if (isset($cart[$wrapper])) {
+            return response()->json(['message' => 'Wrapper already exists in the cart'], 400);
+        }
+    
+        $cart[$wrapper] = $wrapper;
+        session()->put('cart', $cart);
+    
+        return response()->json(['message' => 'Wrapper added to cart'], 200);
+    }
+
+    public function removeFromCart($id) {
+        $wrapper = Wrapper::findOrFail($id);
+        $cart = session()->get('cart', []);
+    
+        if (isset($cart[$wrapper])) {
+            unset($cart[$wrapper]);
+        session()->put('cart', $cart);
+    
+        return response()->json(['message' => 'Wrapper removed from cart'], 200);
+        }
+
+    }
     /**
      * Display a listing of the resource.
      */
